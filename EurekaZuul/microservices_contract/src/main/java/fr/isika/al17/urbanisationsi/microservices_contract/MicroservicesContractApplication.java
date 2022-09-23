3dd40fd82be338896311f10402501d561f387e1e
Farah404 <96427557+Farah404@users.noreply.github.com>
2022-09-23 18:07:06 +0200

    ms-product.properties

--- EurekaZuul/microservices_contract/src/main/java/fr/isika/al17/urbanisationsi/microservices_contract/MicroservicesContractApplication.java
+++ EurekaZuul/microservices_contract/src/main/java/fr/isika/al17/urbanisationsi/microservices_contract/MicroservicesContractApplication.java
@@ -3,7 +3,10 @@ package fr.isika.al17.urbanisationsi.microservices_contract;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class MicroservicesContractApplication {
 
 	public static void main(String[] args) {
